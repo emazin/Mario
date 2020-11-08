46503b3247f07ca18e93d6320d2cb8225a888d22
Eugene Mazin <eugene.mazin@gmail.com>
2020-11-07 18:59:35 -0800

    Mario moving right

--- Mario.cpp
+++ Mario.cpp
@@ -2,23 +2,30 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(600, 400), "Mario!");
-    sf::Texture image;
-    image.loadFromFile("mario.png");
-    sf::Sprite mario(image, sf::IntRect(0, 98, 80, 80));
-    mario.setPosition(0, 300);
+	sf::RenderWindow window(sf::VideoMode(600, 400), "Mario!");
+	sf::Texture image;
+	image.loadFromFile("mario.png");
+	sf::Sprite mario(image, sf::IntRect(0, 98, 80, 80));
+	mario.setPosition(0, 300);
 
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-        window.draw(mario);
-        window.display();
-    }
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
 
-    return 0;
+			if (event.type == sf::Event::KeyPressed)
+			{
+				if (event.key.code == sf::Keyboard::Right)
+					mario.move(5, 0);
+			}
+		}
+		window.clear();
+		window.draw(mario);
+		window.display();
+	}
+
+	return 0;
 }
